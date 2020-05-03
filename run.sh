9f2598cbcbc909ef2221fa2578f86a2796ca9f04
Fuaerlive <64351016+Fuaerlive@users.noreply.github.com>
2020-05-03 21:28:19 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,8 +9,6 @@ Listen = :${PORT}
 ; 是否开启 Debug
 Debug = false
 ; Session 密钥, 一般在首次启动时自动生成
-; 5201314的md5加密密文为723d505516e0c197e42a6be3c0af910e
-; 搭配cloudreve.db 默认关闭注册 管理员为 admin@cloudreve.org / cloudreve@2020
 SessionSecret = 723d505516e0c197e42a6be3c0af910e
 ; Hash 加盐, 一般在首次启动时自动生成
 HashIDSalt = 723d505516e0c197e42a6be3c0af910e
